bb962b770391daf1de2cfe09b3eacf91e261dcca
Antonio Carrasco <wunderton@gmail.com>
2020-06-22 23:31:36 +0200

    Subida de seguridad

--- src/diccionariocliente/DiccionarioCliente.java
+++ src/diccionariocliente/DiccionarioCliente.java
@@ -18,62 +18,66 @@ public class DiccionarioCliente {
      * @param args the command line arguments
      */
     public static void main(String[] args) throws IOException {
-         String[] palabraConEspacios;
+         
 
-//localhost: 127.0.0.1
-//Mi ip: 85.55.245.139
-//Cuvi: 154.62.44.50
-//Olivencia: 154.47.140.75
-//Raul: 176.57.103.161
+    //localhost: 127.0.0.1
+    //Mi ip: 85.55.245.139
+    //Cuvi: 154.62.44.50
+    //Olivencia: 154.47.140.75
+    //Raul: 176.57.103.161
 
         String hostName = "127.0.0.1";
         int portNumber = 3478;
         String nombre = "Carrasco";
      
         try (
-            Socket kkSocket = new Socket(hostName, portNumber);
-            PrintWriter out = new PrintWriter(kkSocket.getOutputStream(), true);
+            Socket cliente = new Socket(hostName, portNumber);
+            PrintWriter out = new PrintWriter(cliente.getOutputStream(), true);
             BufferedReader in = new BufferedReader(
-                new InputStreamReader(kkSocket.getInputStream()));
+                new InputStreamReader(cliente.getInputStream()));
         ) {
-            BufferedReader stdIn = new BufferedReader(new InputStreamReader(System.in));
-            String fromServer;
-            String fromUser;
+            BufferedReader fromKeyboard = new BufferedReader(new InputStreamReader(System.in));
+            String mensajeDesdeServidor = "";
+            String mensajeDelUsuario = "";
             String inicioSesion = "PROTOCOL_PSP_JUNIO#HELLO_ITS_ME#"+nombre;
+            System.out.println("Cliente: PROTOCOL_PSP_JUNIO#HELLO_ITS_ME#" +nombre);
             out.println(inicioSesion);
 
-            while ((fromServer = in.readLine()) != null) {
-                System.out.println(fromServer);
-                fromUser = stdIn.readLine();
-                if (fromUser != null) {
-                    //System.out.println();
-                    if (fromUser.contains("bye") || fromUser.contains("adios")){
-                        out.println("PROTOCOL_PSP_JUNIO#GOODBYE_MY_LOVE#BY#" + nombre);
-                        System.exit(-1);   
-                        }
-                    }
-         
+            mensajeDesdeServidor = in.readLine();
+            System.out.println("Server: " +mensajeDesdeServidor);
+
+            while (mensajeDelUsuario != "bye") {
+                mensajeDelUsuario = fromKeyboard.readLine();
+                if (mensajeDelUsuario.contains("bye")) {
+                    out.println("PROTOCOL_PSP_JUNIO#GOODBYE_MY_LOVE#BY#" + nombre);
+                    System.out.println("Cliente: PROTOCOL_PSP_JUNIO#GOODBYE_MY_LOVE_BY" + nombre);
+                    mensajeDesdeServidor = in.readLine();
+                    System.out.println("Server: " + mensajeDesdeServidor);
+                    cliente.close();   
+                } else {
+                
                     int contadorEspacios = 0;
+                    String[] palabras = mensajeDelUsuario.split(" ");
 
-                    palabraConEspacios = fromUser.split(" ");
-
-                    for(int i = 0; i < fromUser.length(); i++){
-                        if(fromUser.charAt(i) == ' '){
+                    for(int i = 0; i < mensajeDelUsuario.length(); i++){
+                        if(mensajeDelUsuario.charAt(i) == ' '){
                         contadorEspacios++;
                         }
                     }    
-                    System.out.println(contadorEspacios);
+                    //System.out.println(contadorEspacios);
 
-                    for(int i = 0; i < palabraConEspacios.length; i++) {   
-                        System.out.println(palabraConEspacios[i]);
-                        out.println("PROTOCOL_PSP_JUNIO#ASK_FOR#" + palabraConEspacios[i] + "#BY#" + nombre);
+                    for(int i = 0; i < palabras.length; i++) {   
+                        System.out.println("DEBUG: " + palabras[i]);
+                        out.println("PROTOCOL_PSP_JUNIO#ASK_FOR#" + palabras[i] + "#BY#" + nombre);
+                        System.out.println("Cliente: PROTOCOL_PSP_JUNIO#ASK_FOR#"+palabras[i] +"#BY#" + nombre);
                     }
-                    for(int i  = 0; i < contadorEspacios; ++i){
-                        fromServer = in.readLine();
-                        System.out.println("Server" +fromServer);
+                    for(int i  = 0; i < palabras.length; i++){
+                        mensajeDesdeServidor = in.readLine();
+                        System.out.println("Server" +mensajeDesdeServidor);
                     }
-                    
-            }   
+
+                }   
+            }
         } catch (UnknownHostException e) {
             System.err.println("Don't know about host " + hostName);
             System.exit(1);
@@ -81,6 +85,6 @@ public class DiccionarioCliente {
             System.err.println("Couldn't get I/O for the connection to " +
                 hostName);
             System.exit(1);
+        }
     }
-}
 }
