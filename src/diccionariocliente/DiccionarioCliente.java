9d4bfa4271244598328ceadc87b4a3cb63f2ee4b
Antonio Carrasco <wunderton@gmail.com>
2020-06-22 22:19:33 +0200

    Commit de emergencia. Antes lo he perdido todo, vuelve a funcionar

--- src/diccionariocliente/DiccionarioCliente.java
+++ src/diccionariocliente/DiccionarioCliente.java
@@ -18,13 +18,14 @@ public class DiccionarioCliente {
      * @param args the command line arguments
      */
     public static void main(String[] args) throws IOException {
-         ArrayList<String> palabraConEspacios = new ArrayList<String>();
-//        if (args.length != 2) {
-//            System.err.println(
-//                "Usage: java EchoClient <host name> <port number>");
-//            System.exit(1);
-//        }
-//127.0.0.1
+         String[] palabraConEspacios;
+
+//localhost: 127.0.0.1
+//Mi ip: 85.55.245.139
+//Cuvi: 154.62.44.50
+//Olivencia: 154.47.140.75
+//Raul: 176.57.103.161
+
         String hostName = "127.0.0.1";
         int portNumber = 3478;
         String nombre = "Carrasco";
@@ -51,9 +52,11 @@ public class DiccionarioCliente {
                         System.exit(-1);   
                         }
                     }
-                if (fromUser.contains(" ")){
+         
                     int contadorEspacios = 0;
-                    
+
+                    palabraConEspacios = fromUser.split(" ");
+
                     for(int i = 0; i < fromUser.length(); i++){
                         if(fromUser.charAt(i) == ' '){
                         contadorEspacios++;
@@ -61,32 +64,23 @@ public class DiccionarioCliente {
                     }    
                     System.out.println(contadorEspacios);
 
-                    String[] espacios = fromUser.split(" ");
-        
-                    for(int i = 0; i <= contadorEspacios; i++) {   
-                        palabraConEspacios.add(espacios[i]);
+                    for(int i = 0; i < palabraConEspacios.length; i++) {   
+                        System.out.println(palabraConEspacios[i]);
+                        out.println("PROTOCOL_PSP_JUNIO#ASK_FOR#" + palabraConEspacios[i] + "#BY#" + nombre);
                     }
-
-                    for(int i = 0; i <= contadorEspacios; i++) {   
-                        System.out.println(palabraConEspacios.get(i));
-                        out.println("PROTOCOL_PSP_JUNIO#ASK_FOR#" + palabraConEspacios.get(i) + "#BY#" + nombre);
-                    }
-
-                    for(int i = 0; i < contadorEspacios; i++){
+                    for(int i  = 0; i < contadorEspacios; ++i){
                         fromServer = in.readLine();
-                        System.out.println(fromServer);
+                        System.out.println("Server" +fromServer);
                     }
-                } else out.println("PROTOCOL_PSP_JUNIO#ASK_FOR#" + fromUser + "#BY#" + nombre);
-                    System.out.println("Server: " + fromServer);
-                palabraConEspacios.clear();
+                    
             }   
-        }catch (UnknownHostException e) {
+        } catch (UnknownHostException e) {
             System.err.println("Don't know about host " + hostName);
             System.exit(1);
         } catch (IOException e) {
             System.err.println("Couldn't get I/O for the connection to " +
                 hostName);
             System.exit(1);
-        }
     }
+}
 }
